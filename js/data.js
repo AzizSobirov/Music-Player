2252132a674bb1c4c20660923687f858af207bc2
Aziz Sobirov <66164628+AzizSobirov@users.noreply.github.com>
2021-08-26 17:48:38 +0500

    Add files via upload

--- js/data.js
+++ js/data.js
@@ -2,32 +2,32 @@ let musics = [
 {
     name:"Minelli",
     title:"Rampampam",
-    src:"https://ru-music.com/public/play_mp3.php?id=56297&hash=113fc42235c2b5d0c8efbd1138b70f21be2b0081b20a886627a32e594482d82d&artist=Minelli&title=Rampampam&from_main=1",
+    src:"https://dl5.ru-music.cc/mp3/56297.mp3",
     image:"https://www.cevirce.com/en/translate/wp-content/uploads/2021/05/minelli-rampampam-lyrics-1.jpg",
 },{
     name:"Galibri & Mavik",
     title:"Федерико Феллини",
-    src:"https://ru-music.com/public/play_mp3.php?id=56463&hash=b3765cdd94d0cb24d08ab23079b10225c996ababe6eefa95e54a7ced16e51d3f&artist=Galibri%20%26%20Mavik&title=%D0%A4%D0%B5%D0%B4%D0%B5%D1%80%D0%B8%D0%BA%D0%BE%20%D0%A4%D0%B5%D0%BB%D0%BB%D0%B8%D0%BD%D0%B8&from_main=1",
+    src:"https://dl6.ru-music.cc/mp3/56463.mp3",
     image:"https://i-tsmusic.com/i/img/songs/22038.jpg",
 },{
     name:"T1ONE",
     title:"Двенадцатый",
-    src:"https://ru-music.com/public/play_mp3.php?id=54685&hash=f8e4f77b0f0459be417115b06037311e28497f1f57f7448398c5ccf699ba518d&artist=T1ONE&title=%D0%94%D0%B2%D0%B5%D0%BD%D0%B0%D0%B4%D1%86%D0%B0%D1%82%D1%8B%D0%B9&from_main=1",
+    src:"https://dl6.ru-music.cc/mp3/54685.mp3",
     image:"https://i-tsmusic.com/i/img/songs-normal/20645.webp",
 },{
     name:"Timran & Zell & Batrai Aslai",
     title:"Не Пускайте Танцевать",
-    src:"https://ru-music.com/public/play_mp3.php?id=8225&hash=c2cdb482e555dee0d3f53e54620218ba55d6b35989eef1c07247b1481551954c&artist=Timran&title=%D0%9D%D0%B5%20%D0%9F%D1%83%D1%81%D0%BA%D0%B0%D0%B9%D1%82%D0%B5%20%D0%A2%D0%B0%D0%BD%D1%86%D0%B5%D0%B2%D0%B0%D1%82%D1%8C&from_main=1",
+    src:"https://dl6.ru-music.cc/mp3/8225.mp3",
     image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSFpTIyfh5FECiPzwLurtK-lgli0vqqphR8Ow&usqp=CAU",
 },{
     name:"Bodiev",
     title:"Караван",
-    src:"https://ru-music.com/public/play_mp3.php?id=57172&hash=727d01d2d39010c87207e3ddff6ef8ee1fcd7ec1091e45aa14ee3ee6993083db&artist=Bodiev&title=%D0%9A%D0%B0%D1%80%D0%B0%D0%B2%D0%B0%D0%BD&from_main=1",
+    src:"https://dl5.ru-music.cc/mp3/57172.mp3",
     image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTZw-ESwyxxKYz0rtP5cECtB_D1J5bVDcvoPxm0FJjbraLHou1M-bf2Hloio5tsd6ALNaU&usqp=CAU",
 },{
     name:"Тимур Timbigfamily",
     title:"Оксана Ковалевская",
-    src:"https://ru-music.com/public/play_mp3.php?id=54539&hash=2dca3ae6f267b70b994e8e27fbbcdba09ea0aa87cf8166a199badcef253ecdd8&artist=%D0%A2%D0%B8%D0%BC%D1%83%D1%80%20Timbigfamily%2C%20%D0%9E%D0%BA%D1%81%D0%B0%D0%BD%D0%B0%20%D0%9A%D0%BE%D0%B2%D0%B0%D0%BB%D0%B5%D0%B2%D1%81%D0%BA%D0%B0%D1%8F&title=%D0%90%20%D0%B2%D1%8B%20%D0%BD%D0%B5%20%D1%82%D1%80%D0%BE%D0%B3%D0%B0%D0%B9%D1%82%D0%B5%20%D0%BC%D0%B5%D0%BD%D1%8F&from_main=1",
+    src:"https://dl6.ru-music.cc/mp3/54539.mp3",
     image:"https://muzzona.info/uploads/posts/2020-12/1609162182_mzhsawvvpru.jpg",
 }
 ]
